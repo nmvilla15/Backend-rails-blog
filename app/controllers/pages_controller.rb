f11de175bb35e38869b63fa054a3420a917d14f7
nmvilla15 <niki.villalpando@gmail.com>
2021-04-22 22:11:38 -0500

    completed lesson 6, Rails Basics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,48 @@
 class PagesController < ApplicationController
+    before_action :set_page, only: [:show, :edit, :update, :destroy]
+
     def index
         @pages = Page.all
     end
 
     def show
-        @page = Page.find(params[:id])
+        #set_page
         #render text: @page.title
     end
+
+    def new
+        @page = Page.new
+    end
+
+    def create
+        @page = Page.new(page_params)
+        @page.save
+        redirect_to @page
+    end
+
+    def edit
+        #set_page
+    end
+
+    def update
+        #set_page
+        @page.update(page_params)
+        redirect_to @page
+    end
+
+    def destroy
+        #set_page
+        @page.destroy
+        redirect_to pages_path
+    end
+
+    private
+
+        def page_params
+            params.require(:page).permit(:title, :body, :slug)
+        end
+
+        def set_page
+            @page = Page.find(params[:id])
+        end
 end
